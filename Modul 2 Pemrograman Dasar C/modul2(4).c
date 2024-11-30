e60fcfe3d1766047f2904be7e5669550f433e878
Lydya Husni Anggina Harahap <lydyahusni@gmail.com>
2024-11-30 23:23:20 +0700

    Add files via upload

---
+++ Modul 2 Pemrograman Dasar C/modul2(4).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int p = 10;
+    int l = 5;
+    int luaspersegipanjang = (p * l);
+
+    printf("luas persegi panjang: %i", luaspersegipanjang);
+
+    return 0;
+}
