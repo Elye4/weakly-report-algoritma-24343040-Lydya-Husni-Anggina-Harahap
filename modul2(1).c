ce2c6b3cab2753821ac3b17b018cb776da0322d3
Lydya Husni Anggina Harahap <lydyahusni@gmail.com>
2024-11-30 23:06:21 +0700

    Add files via upload

---
+++ modul2(1).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    char nama[100];
+
+     printf("hello, siapa nama lengkapmu?");
+     scanf("%s", &nama);
+
+     printf("Selamat Datang %s Dalam Pemograman C", nama);
+
+     return 0;
+
+}
